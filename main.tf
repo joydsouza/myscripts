bfbf50867b320045f18001969cc9de806dc21de3
root <root@ip-172-31-23-154.ap-northeast-2.compute.internal>
2018-11-22 08:26:25 +0000

    add container

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ resource "docker_image" "image_id" {
 resource "docker_image" "image_id2" {
   name = "nginx:latest"
 }
+# Start a container
+resource "docker_container" "ghost" {
+  name  = "container1"
+  image = "${docker_image.image_id.latest}"
+}
+# Start a container
+resource "docker_container" "nginx" {
+  name  = "container2"
+  image = "${docker_image.image_id2.latest}"
+}
+
